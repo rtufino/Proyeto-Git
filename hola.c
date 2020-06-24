e6c17d5646af8aa627bbf86542b56656e3ade5b3
Rodrigo Tufiño <rtufino@ups.edu.ec>
2020-06-24 08:17:37 -0500

    Agregada interfaz en C

---
+++ hola.c
@@ -0,0 +1,12 @@
+/*
+ * Curso de introducción a C/C++
+ * Autor: Rodrigo Tufiño <rtufino@ups.edu.ec>
+ * Fecha: marzo 2020
+ */
+
+#include <stdio.h>
+
+int main(){
+    printf("Hola mundo desde C!");
+    return 0;
+}
--- script.py
+++ script.py
@@ -3,7 +3,8 @@
 # Fecha: 24-06-2020
 
 def main():
-    print("Hola Mundo!")
+    nombre = input("Ingresa tu nombre: ")
+    print("Hola", nombre, "Bienvenido a Git!")
     
 if __name__ == "__main__":
     main()
